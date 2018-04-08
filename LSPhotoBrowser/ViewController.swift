9c42dd0d3b80a2811bcb9be2e10b08ede3b873f1
linsheng <linsheng@xrjiot.com>
2018-04-08 19:27:23 +0800

    Add: project & pods

---
+++ LSPhotoBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LSPhotoBrowser
+//
+//  Created by Lin Sheng on 2018/4/8.
+//  Copyright © 2018年 linsheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
